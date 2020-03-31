917a94b7748be0c182e870de2d387bf3c72c2ae2
Luke Shimanuki <luke.shimanuki@gmail.com>
2020-03-31 14:47:27 -0400

    testing mic input

--- client.cpp
+++ client.cpp
@@ -37,7 +37,7 @@ struct Remote {
 			if (err.value() != 0)
 				std::cerr << "Error sending udp to " << endpoint.address() << '\n' << err << '\n';
 			//else std::cout << '.' << std::flush;
-			else std::cout << "sending udp to " << endpoint.address() << ' ' << endpoint.port() << '\n';
+			//else std::cout << "sending udp to " << endpoint.address() << ' ' << endpoint.port() << '\n';
 		}
 		udp::endpoint endpoint;
 	} send_handler;
@@ -60,9 +60,8 @@ int sink_cb(
 	PaStreamCallbackFlags statusFlags,
 	void* userData
 ) {
-	//std::memcpy(outputBuffer, mic_buffers[(buffer_idx + num_buffers - 1) % num_buffers], buffer_size * 2);
-	//std::cout << ((int16_t*)outputBuffer)[0] << '\n';
-	//return 0;
+	std::memcpy(outputBuffer, mic_buffers[(buffer_idx + num_buffers - 1) % num_buffers], buffer_size * 2);
+	return 0;
 
 	int16_t* const out = (int16_t*)outputBuffer;
 	for (unsigned long frame = 0; frame < framesPerBuffer; ++frame) {
@@ -92,6 +91,7 @@ int source_cb(
 	PaStreamCallbackFlags statusFlags,
 	void* userData
 ) {
+	std::cout << ((int16_t*)inputBuffer)[0] << '\n';
 	std::memcpy(mic_buffers[buffer_idx], inputBuffer, framesPerBuffer * sizeof(int16_t));
 	mic_buffers[buffer_idx][buffer_size] = buffer_idx;
 	for (const auto& remote : remotes)
